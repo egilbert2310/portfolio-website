8934881341dd14bffa9cbd2fb504bee9c0aa6446
Eric <ericdgilbert2310@gmail.com>
2023-05-07 11:23:59 -0400

    styled nav

--- src/components/nav.component.jsx
+++ src/components/nav.component.jsx
@@ -1,14 +1,12 @@
 function Nav (){
     return(
         <nav className="nav">
-
-        <a href="#home" class="w3-bar-item w3-button">HOME</a>
-        <a href="#about" class="w3-bar-item w3-button w3-hide-small"
-        > ABOUT</a >
-        <a href="#portfolio" class="w3-bar-item w3-button w3-hide-small"
-          >PORTFOLIO</a >
-        <a href="#contact" class="w3-bar-item w3-button w3-hide-small">
-             CONTACT</a>
+        <ul>
+        <li><a href="#home" class="w3-bar-item w3-button">HOME</a></li>
+        <li><a href="#about" class="w3-bar-item w3-button w3-hide-small"> ABOUT</a ></li>
+        <li><a href="#portfolio" class="w3-bar-item w3-button w3-hide-small">PORTFOLIO</a ></li>
+        <li><a href="#contact" class="w3-bar-item w3-button w3-hide-small">CONTACT</a></li>
+        </ul>
         </nav>
     )
 }
