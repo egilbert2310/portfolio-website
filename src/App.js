bd9fc9e93a2545ebbdb0a6b05677db3b9eb1a5ad
Eric <ericdgilbert2310@gmail.com>
2023-05-11 20:24:21 -0400

    started main header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Card from './components/card.component';
+import MainHeader from './components/main_header.component';
 import Nav from './components/nav.component';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <MainHeader jobtitle={"Data Engineer"}/>
     </div>
   );
 }
---
+++ src/components/main_header.component.jsx
@@ -0,0 +1,9 @@
+export default function MainHeader({jobtitle}){
+    return(
+        <div className="main-header">
+            <h1>Hello, I'm</h1>
+            <h2>Eric Gilbert</h2>
+            <p>{jobtitle}</p>
+        </div>
+    )
+}
