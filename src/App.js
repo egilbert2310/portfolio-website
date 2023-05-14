c05d9e77fd6608ab81a4c872fc4d17bef084639a
Eric <ericdgilbert2310@gmail.com>
2023-05-14 12:16:42 -0400

    started about section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import MainHeader from './components/main_header.component';
 import Nav from './components/nav.component';
+import About from './components/about.component';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <MainHeader jobtitle={"Data Engineer"}/>
+      <About/>
     </div>
   );
 }
