bfc05864e930de4947a7dc977075a436d1eb5a46
Eric <ericdgilbert2310@gmail.com>
2023-05-21 10:39:45 -0400

    layout of contact form changed

--- src/components/contact.component.jsx
+++ src/components/contact.component.jsx
@@ -5,16 +5,16 @@ export default function ContactForm(){
                 <h2>Contact</h2>
             <div className="contact-inputs">
                 <div>
-                    <label htmlFor="name">Name*</label>
-                    <input type="text" name="first-name" id="name" placeholder="First Name" required/>
-                    <input type="text" name="last-name" id="name" placeholder="Last Name" required/>
+                    <label htmlFor="first-name">Name *</label>
+                    <input type="text" name="first-name" id="first-name" placeholder="First Name" required/>
+                    <input type="text" name="last-name" id="last-name" placeholder="Last Name" required/>
                 </div>
                 <div>
-                    <label htmlFor="email">Email*</label>
+                    <label htmlFor="email">Email *</label>
                     <input type="email" name="email" id="email" required/>
                 </div>
                 <div>
-                    <label htmlFor="message">Message*</label>
+                    <label htmlFor="message">Message *</label>
                     <textarea name="message" id="message" cols="30" rows="10"></textarea>
                 </div>
                 <button type="submit">Submit</button>
