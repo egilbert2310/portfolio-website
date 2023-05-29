830f2932678fe5623d90486571c734a1943d2a0d
Eric <ericdgilbert2310@gmail.com>
2023-05-29 15:31:21 -0400

    animation is beginning to take shape

--- src/components/animations/TextAnimation.jsx
+++ src/components/animations/TextAnimation.jsx
@@ -1,19 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled, { keyframes } from "styled-components";
+import { useState } from "react";
+import { useInterval } from "react-use";
+
 
 export default function TextAnimation(){
-    return <Wrapper>Data Engineer</Wrapper>
+    const title1 = 'Data Engineer'
+    const title2 = 'Software Engineer'
+
+    const title_arr = ['Data Engineer', 'Software Engineer', 'Data Scientist', 'Data Analyst', 'A Cool Guy']
+    const [titles, setTitle] = useState(title1)
+    const [count, setCount] = useState(0)
+    const [play, setPlay] = useState(false)
+
+    useInterval(
+        () => {
+            setTitle(title1)
+            setCount(count + 1)
+
+            if (count === 1){
+                setCount(0)
+                setTitle(title2)
+            }
+        },
+        play ? 6000 : null
+    )
+
+    useEffect(() =>{
+        const timer = setTimeout(() => {
+            setTitle(title2)
+            setPlay(true)
+        }, 4000)
+
+        return () => clearTimeout(timer)
+    }, [])
+
+    return (
+    <Wrapper>{titles}</Wrapper>
+ )
 }
 
 const animation = keyframes`
     0% {opacity: 0;}
-    100% {opacity: 1}
+    25% {opacity: 1;}
+    75% {opacity: 1;}
+    100% {opacity: 0}
 `
 
-const Wrapper = styled.span`
+const Wrapper = styled.p`
     opacity: 0;
     animation-name: ${animation};
-    animation-duration: 6s;
+    animation-duration: 4s;
+    animation-fill-mode: forwards;
+    animation-iteration-count: infinite;
     text-align: center;
     font-size: 2.5rem;
+    display: block;
 `
--- src/sections/main_header.component.jsx
+++ src/sections/main_header.component.jsx
@@ -5,35 +5,31 @@ import TextAnimation from "../components/animations/TextAnimation"
 
 export default function MainHeader(){
     return(
-        <MainHeaderContainer>
+        <div>
                 <Title>Hello, I'm</Title>
                 <Name>Eric Gilbert</Name>
                 <TextAnimation/>
-        </MainHeaderContainer>
+        </div>
     )
 }
 
-const MainHeaderContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 2rem;
-`
+
+
 
 const Title = styled.h1`
+    display: block;
     color: black;
     font-size: 5rem;
     text-align: center;
     margin-bottom: 0%;
     padding-top: 0%;
-    display: block;
 `
 
 const Name = styled.h2`
+    display:block;
     color: rgb(75, 26, 133);
     font-size: 3.5rem;
     text-align: center;
     margin: 0%;
     padding: 0%;
-    display:block;
 `
